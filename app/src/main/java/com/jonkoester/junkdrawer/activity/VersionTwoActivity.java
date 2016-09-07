2388bb97e01bc00c6aa489ef307120270d05ae53
Jonathan Koester <Jonathan.D.Koester@ehi.com>
2016-09-07 12:54:37 -0500

    - Fixed some positioning issues with LEFT and TOP - Added some examples of them in action

--- app/src/main/java/com/jonkoester/junkdrawer/activity/VersionTwoActivity.java
+++ app/src/main/java/com/jonkoester/junkdrawer/activity/VersionTwoActivity.java
@@ -26,6 +26,10 @@ public class VersionTwoActivity extends Activity {
     TableLayout personalTableLayout;
     @BindView(R.id.act_vTwo_tv2)
     TextView testTwo;
+    @BindView(R.id.act_vTwo_bdayTV)
+    TextView bdayTV;
+    @BindView(R.id.act_vTwo_zipTV)
+    TextView zipTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +46,8 @@ public class VersionTwoActivity extends Activity {
         testQ.add(new TutorialDialogModel(genderTV, "Gender", "I remember the days when there were only two options", TutorialDialogModel.Direction.RIGHT, null, TutorialDialogModel.YAlignment.CENTER));
         testQ.add(new TutorialDialogModel(testTwo, "Test TV", "This is off in East Jesus for convenience", TutorialDialogModel.Direction.RIGHT, null, TutorialDialogModel.YAlignment.BOTTOM));
         testQ.add(new TutorialDialogModel(personalTableLayout, "Section Test", "Holy shit look at this section of the app!", TutorialDialogModel.Direction.BOTTOM, TutorialDialogModel.XAlignment.CENTER, null));
+        testQ.add(new TutorialDialogModel(bdayTV, "Birthday!", "Look everyone! My birthday is on March 30th!", TutorialDialogModel.Direction.LEFT, null, TutorialDialogModel.YAlignment.TOP));
+        testQ.add(new TutorialDialogModel(zipTV, "Zip Code", "This is my zip code. It is boring", TutorialDialogModel.Direction.LEFT, null, TutorialDialogModel.YAlignment.CENTER));
 
         addContentView(new VersionTwoOverlay(this, testQ), new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
     }
